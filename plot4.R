34347bbf2088541f31d7268b145072ee5571d167
rsalama <roberto.salama@gmail.com>
2018-03-12 21:20:58 -0400

    fixed legend box

--- plot4.R
+++ plot4.R
@@ -8,7 +8,6 @@ colNames <- c("Date","Time","Global_active_power","Global_reactive_power","Volta
 df <- read.table("household_power_consumption.txt", sep=";", skip=66637, nrows = 2880, col.names = colNames,  na.strings = "?")
 df$DateTime <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
 
-## Generate PNG plot
 png(filename="plot4.png")
 par(mfrow = c(2, 2))
 with(df, {
@@ -19,7 +18,7 @@ with(df, {
   plot(DateTime, Sub_metering_1,type="l",col="gray", ylab = "Energy sub metering", xlab = "")
   lines(DateTime, Sub_metering_2, col="red")
   lines(DateTime, Sub_metering_3, col="blue")
-  legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("gray", "red", "blue"), bty = "n")
+  legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("gray", "red", "blue"), lty = 1, bty = "n")
   
   plot(DateTime, Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
 })
